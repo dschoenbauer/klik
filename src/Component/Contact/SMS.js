ecfb0495416e693ce9a5bccd902ae13968752baa
David Schoenbauer <dschoenbauer@gmail.com>
2020-11-08 06:35:28 -0600

    task: #13 update icon to match others

--- src/Component/Contact/SMS.js
+++ src/Component/Contact/SMS.js
@@ -3,7 +3,7 @@ export function SMS(props) {
 	const { telephone, className, title = "Send a text", message } = props;
 	const smsLink = `sms:+1${telephone.replace(/[\D\.]/g, "")}&body=${encodeURI(message)}`;
 	return <a className={className} title={title} target="_blank" rel="noopener noreferrer" href={smsLink}>
-		<i className="fa fa-commenting-o"></i>
+		<i className="fa fa-commenting"></i>
 		{props.children}
 	</a>
 }
