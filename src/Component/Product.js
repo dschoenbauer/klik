90df421ac483cc1812d9ac201c0cf498a637be9e
David Schoenbauer <dschoenbauer@gmail.com>
2020-11-08 04:19:24 -0600

    task: #5 complete

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -4,7 +4,7 @@ import "./Product.scss";
 export default class Product extends Component {
 	render() {
 		const { name, price, includes = []} = this.props;
-		const title = price ? `${name}: $${price}`:name;
+		const title = price ? `${name} $${price}`:name;
 		return <div className='product'>
 			<h2 className="product__title">{title}</h2>
 			<ul className="product__includes">
