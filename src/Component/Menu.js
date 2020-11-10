9253be6af4143ee448f5d80990c041d2ae8998a1
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2020-11-09 20:54:37 -0600

    Task #4 Add contact form

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -4,17 +4,18 @@ import { Menu as MenuData } from '../Data/Menu.js';
 import "./Menu.scss";
 
 export default class Menu extends Component {
-	getMenuItem(data, idx) {
+	getMenuItem(data, idx, parentRoot = null) {
 		const { label, route, children = [] } = data;
 		let childrenDom = "";
 		if (children && Array.isArray(children) && children.length > 0) {
 			const items = data.children.map((menu, idx) => {
-				return this.getMenuItem(menu, idx, route);
+				return this.getMenuItem(menu, idx, "/" + route);
 			})
 			childrenDom = <ul className="menu__sub-menu">{items}</ul>;
 		}
+		const path = (parentRoot ? parentRoot : "") + `/${route}`;
 		return <li key={idx} className="menu__item">
-			<NavLink  className="menu__link" activeClassName="menu__link--active" to={route}>{label}</NavLink >
+			<NavLink  className="menu__link" activeClassName="menu__link--active" to={path}>{label}</NavLink >
 			{childrenDom}
 		</li>;
 	}
