34397a7e1fb678b707c6428c4c39ba81354ef383
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2020-11-11 15:57:25 -0600

    Correct deployment errors

--- src/Component/Contact/SMS.js
+++ src/Component/Contact/SMS.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export function SMS(props) {
 	const { telephone, className, title = "Send a text", message } = props;
-	const smsLink = `sms:+1${telephone.replace(/[\D.]/g, "")}&body=${encodeURI(message)}`;
+	const smsLink = `sms:+1${telephone.replace(/[\D.]/g, "")}?body=${encodeURI(message)}`;
 	return <a className={className} title={title} target="_blank" rel="noopener noreferrer" href={smsLink}>
 		<i className="fa fa-commenting"></i>
 		{props.children}
