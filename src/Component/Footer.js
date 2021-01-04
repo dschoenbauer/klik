ff297fb438ee8da3f62a81afb5c1dbae3af9d691
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2021-01-04 16:13:25 -0600

    Add copyright

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,8 @@ export default class Footer extends Component {
 	
 	render(){
 		return <footer className="footer">
-
+			<p>Copyright © 2020 by Krystle Foley</p>
+			<p>All Rights Reserved.</p>
 		</footer>;
 	}
 }
